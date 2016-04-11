93629d1afe4d2e47d29dea9aa88d9067b9683df0
Ali Rashed <Ali Rashed>
2016-04-11 17:07:21 -0500

    markov chain generation for arb. length + tests

--- markov.py
+++ markov.py
@@ -1,7 +1,7 @@
 import random
 
 class MarkovState:
-	def __init__(self, state):
+	def __init__(self, state): #note, one element tuple being treated as a string, maybe a problem?, could just require size > 1
 		self.state = state
 		self.follow = {}
 		self.total = 0
@@ -9,6 +9,13 @@ class MarkovState:
 		return hash(self.state)
 	def __eq__(self, another):
 		return hasattr(another, 'state') and self.state == another.state
+	def __str__(self):
+		result = ""
+		for x in range(0, len(self.state)):
+			result += self.state[x]
+			if(x < len(self.state) - 1):
+				result += " "
+		return result
 
 	def is_matching(self, other):
 		return self.state == other
@@ -21,12 +28,24 @@ class MarkovState:
 		self.total += 1
 
 	def generate_next(self):
+		if self.total == 0:
+			return None
+
 		goal = random.randint(0, self.total)
 
 		for key, value in self.follow.items():
 			goal -= value
 			if goal <= 0:
 				return key
+
+	def adjust_state(self, new):
+		adjusted = []
+		for x in range(1, len(self.state)):
+			adjusted.append(self.state[x])
+		adjusted.append(new)
+
+		return MarkovState(tuple(adjusted))
+
 
 class MarkovEngine:
 	def __init__(self):
@@ -51,3 +70,37 @@ class MarkovEngine:
 				return True
 		else:
 			return False
+
+	def get_state(self, to_match):
+		for s in self.states:
+			if s == to_match:
+				return s
+		return None
+
+	def get_random_state(self):
+		index = random.randint(0, len(self.states) - 1)
+		return self.states[index]
+
+	def generate_chain(self, length):
+		result = ""
+		current = self.get_random_state()
+		result += str(current)
+		total_length = len(result)
+
+		while total_length < length:
+			next = current.generate_next()
+
+			if next != None:
+				total_length += len(next) + 1
+				if total_length <= length:
+					result += " " + next
+					tmp = current.adjust_state(next)
+					current = self.get_state(tmp)
+			
+			if next == None or current == None:
+				total_length = length
+
+		return result
+
+
+
--- tests.py
+++ tests.py
@@ -3,8 +3,8 @@ from markov import *
 
 class TestStateConstruction(unittest.TestCase):
   def test_construct(self):
-    x = MarkovState(("hello"))
-    self.assertEqual(x.state, ("hello"))
+    x = MarkovState(("hello",))
+    self.assertEqual(x.state, ("hello",))
     self.assertEqual(x.total, 0)
 
   def test_tuple_member(self):
@@ -14,28 +14,37 @@ class TestStateConstruction(unittest.TestCase):
 
 class TestStateComparison(unittest.TestCase):
   def test_matching_true(self):
-    x = MarkovState(("hello"))
-    self.assertTrue(x.is_matching("hello"))
+    x = MarkovState(("hello",))
+    self.assertTrue(x.is_matching(("hello",)))
 
   def test_matching_false(self):
-    x = MarkovState(("hello"))
-    self.assertFalse(x.is_matching("world"))
+    x = MarkovState(("hello",))
+    self.assertFalse(x.is_matching(("world",)))
+
+class TestStateString(unittest.TestCase):
+  def test_to_string_1(self):
+    x = MarkovState(("hello", "world"))
+    self.assertEqual(str(x), "hello world")
+
+  def test_to_string_2(self):
+    x = MarkovState(("hello",))
+    self.assertEqual(str(x), "hello")
 
 class TestStateAppend(unittest.TestCase):
   def test_append_1(self):
-    x = MarkovState(("hello"))
+    x = MarkovState(("hello",))
     x.append("foo")
     self.assertEqual(x.total, 1)
 
   def test_append_2(self):
-    x = MarkovState(("hello"))
+    x = MarkovState(("hello",))
     self.assertEqual(x.total, 0)
     x.append("foo")
     x.append("foo")
     self.assertEqual(x.total, 2)
 
   def test_append_3(self):
-    x = MarkovState(("hello"))
+    x = MarkovState(("hello",))
     self.assertEqual(x.total, 0)
     x.append("foo")
     x.append("bar")
@@ -44,44 +53,60 @@ class TestStateAppend(unittest.TestCase):
 
 class TestStateGenerateNext(unittest.TestCase):
   def test_generate_next_1(self):
-    x = MarkovState(("hello"))
+    x = MarkovState(("hello",))
     x.append("foo")
     self.assertEqual(x.generate_next(), "foo")
 
 class TestStateHashAndEq(unittest.TestCase):
   def test_eq_true(self):
-    x = MarkovState(("hello"))
+    x = MarkovState(("hello",))
     x.append("foo")
-    y = MarkovState(("hello"))
+    y = MarkovState(("hello",))
     self.assertTrue(x == y)
 
   def test_eq_false(self):
-    x = MarkovState(("hello"))
+    x = MarkovState(("hello",))
     x.append("foo")
-    y = MarkovState("world")
+    y = MarkovState("world",)
     y.append("foo")
     self.assertFalse(x == y)
 
   def test_hash_true(self):
-    x = MarkovState(("hello"))
+    x = MarkovState(("hello",))
     x.append("foo")
-    y = MarkovState(("hello"))
+    y = MarkovState(("hello",))
     d = {}
     d[x] = 5
     self.assertTrue(y in d)
 
   def test_hash_false(self):
-    x = MarkovState(("hello"))
+    x = MarkovState(("hello",))
     x.append("foo")
-    y = MarkovState(("world"))
+    y = MarkovState(("world",))
     d = {}
     d[x] = 5
     self.assertFalse(y in d)
 
+class TestStateAdjust(unittest.TestCase):
+  def test_adjust_1(self):
+    x = MarkovState(("hello",))
+    y = x.adjust_state("foo")
+    self.assertEqual(y, MarkovState(("foo",)))
+
+  def test_adjust_2(self):
+    x = MarkovState(("hello", "world"))
+    y = x.adjust_state("foo")
+    self.assertEqual(y, MarkovState(("world", "foo")))
+
+  def test_adjust_3(self):
+    x = MarkovState(("hello", "world", "lorem"))
+    y = x.adjust_state("foo")
+    self.assertEqual(y, MarkovState(("world", "lorem", "foo")))
+
 class TestEngineAppend(unittest.TestCase):
   def test_append_1(self):
     m = MarkovEngine()
-    m.append_instance(("hello"), "blah")
+    m.append_instance(("hello",), "blah")
     self.assertEqual(len(m.states), 1)
 
   def test_append_2(self):
@@ -90,25 +115,69 @@ class TestEngineAppend(unittest.TestCase):
 
   def test_append_3(self):
     m = MarkovEngine()
-    m.append_instance(("hello"), "blah")
-    m.append_instance(("hello"), "bleh")
+    m.append_instance(("hello",), "blah")
+    m.append_instance(("hello",), "bleh")
     self.assertEqual(len(m.states), 1)
 
   def test_append_4(self):
     m = MarkovEngine()
-    m.append_instance(("hello"), "blah")
-    m.append_instance(("world"), "blah")
+    m.append_instance(("hello",), "blah")
+    m.append_instance(("world",), "blah")
     self.assertEqual(len(m.states), 2)
 
 class TestEngineExists(unittest.TestCase):
   def test_exist_empty(self):
     m = MarkovEngine()
-    self.assertFalse(m.exists("hello"))
+    self.assertFalse(m.exists(("hello",)))
 
   def test_exist_true_1(self):
     m = MarkovEngine()
-    m.append_instance(("hello"), "blah")
-    self.assertTrue(m.exists("hello"))
+    m.append_instance(("hello",), "blah")
+    self.assertTrue(m.exists(("hello",)))
+
+class TestEngineGeneration(unittest.TestCase):
+  def test_random_state_1(self):
+    m = MarkovEngine()
+    m.append_instance(("hello",), "blah")
+    x = MarkovState(("hello",))
+    self.assertEqual(x, m.get_random_state())
+
+  def test_random_state_2(self):
+    m = MarkovEngine()
+    m.append_instance(("hello",), "foo")
+    m.append_instance(("hello",), "bar")
+    m.append_instance(("hello",), "foobar")
+    x = MarkovState(("hello",))
+    self.assertEqual(x, m.get_random_state())
+
+  def test_random_state_3(self): #hacky test, maybe fix this?
+    m = MarkovEngine()
+    m.append_instance(("hello",), "foo")
+    m.append_instance(("world",), "bar")
+    x = MarkovState(("hello",))
+    y = MarkovState(("world",))
+    result = m.get_random_state()
+    self.assertTrue(x == result or y == result)
+
+  def test_generate_chain_1(self):
+    m = MarkovEngine()
+    m.append_instance(("hello", "world"), "foo")
+    result = m.generate_chain(100)
+    self.assertEqual(result, "hello world foo")
+
+  def test_generate_chain_2(self):
+    m = MarkovEngine()
+    m.append_instance(("hello", "world"), "foo")
+    m.append_instance(("world", "foo"), "bar")
+    result = m.generate_chain(100)
+    self.assertEqual(result, "hello world foo bar")
+
+  def test_generate_chain_2(self): #hacky test, maybe fix this?
+    m = MarkovEngine()
+    m.append_instance(("hello", "world"), "foo")
+    m.append_instance(("world", "foo"), "bar")
+    result = m.generate_chain(100)
+    self.assertTrue(result == "hello world foo bar" or result == "world foo bar")
 
 if __name__ == '__main__':
     unittest.main()
