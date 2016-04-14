9f0006251f4b578ac1731f4fd25ed57c9f0b63e1
Ali Rashed <alitarekrashed@gmail.com>
2016-04-13 21:18:32 -0500

    parsing through twitter archive to generate chains

--- handler.py
+++ handler.py
@@ -1,4 +1,6 @@
 from markov import *
+import json
+import glob
 
 def text_dump(filename):
 	f = open(filename, 'r')
@@ -14,3 +16,22 @@ def text_dump(filename):
 		m.append_instance((test[x], test[x+1]), test[x+2])
 
 	print(m.generate_chain(200))
+
+def twitter_archive(filename):
+	allFiles = glob.glob(filename + '/data/js/tweets/*.js')
+	tweets_decoded = []
+	for month in allFiles:
+		tweet_file = open(month)
+		stringFile = "".join(tweet_file.readlines()[1:])
+		temp = json.loads(stringFile)
+		for tweet in temp:
+			tweets_decoded.append(tweet['text'])
+
+	m = MarkovEngine()	
+	f = open('output.txt', 'w')
+	for tweet in tweets_decoded:
+		t = tweet.split(' ')
+		for x in range(0, len(t) - 2):
+			m.append_instance((t[x], t[x+1]), t[x+2])
+
+	print(m.generate_chain(140))
